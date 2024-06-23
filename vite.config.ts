7f6b2e00c7cc85101c399b8bea5ad1bcac46f684
gamberfox <gamberfox@gmail.com>
2024-06-23 09:47:45 -0500

    Setup GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"./gamberfox.github.io",
+  base:"/gamberfox.github.io",
+    
   build: {
+    outDir:'dist',
     rollupOptions: {
       external: [
         //'vue', // Exclude 'vue' from the bundle

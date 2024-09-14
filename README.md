72678392ed34203ab1716bcf1f0fe14510641519
gamberfox <gamberfox@gmail.com>
2024-09-13 19:47:36 -0500

    AI project working

--- README.md
+++
@@ -1,2 +0,0 @@
-# gamberfox.github.io
-personal portfolio
--- index.test.js
+++
@@ -1,6 +0,0 @@
-
-
-//const{resources}=require("./src/i18n");
-
-//test("there are 3 languages",()=>{expect(typeof(resources)).toBe("object")});
-test("test1",()=>{expect(2+2).toBe(4)});
